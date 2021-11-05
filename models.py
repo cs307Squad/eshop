82e6f0b12c81b9af20158fe0b0aec2e8f6308f3f
nguynguy1 <75349287+nguynguy1@users.noreply.github.com>
2021-11-05 16:44:07 -0400

    Update models.py

--- models.py
+++ models.py
@@ -15,9 +15,15 @@ except connector.Error as err:
 db_cursor = db_connection.cursor(buffered=True)
 db_cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED")
 db_cursor.execute("USE eshop")
-get_login_sql = "SELECT username, password FROM Loginstuff WHERE username = %s AND password = %s "
+get_login_cus_sql = "SELECT User_Name, Password FROM Customer WHERE User_Name = %s AND Password = %s "
+get_login_mer_sql = "SELECT User_Name, Password FROM Merchant WHERE User_Name = %s AND Password = %s "
 get_customer_sql = "SELECT Customer_ID FROM Customer WHERE User_Name = %s"
-update_rating_sql = "INSERT INTO Reviews(Product_Code, Reviews, Customer_ID, Score) VALUES (%s, %s, (SELECT Customer_ID FROM Customer WHERE User_Name = %s), %s)"
+update_product_sql = "UPDATE Product SET Rating = CONCAT(SUBSTR(ROUND((SELECT Total_Score / Score_Count FROM (SELECT * FROM Product) a WHERE a.Product_Code = %s),1),0,3)," \
+                     "' out of 5 stars') WHERE Product_Code = %s"
+check_product_sql = "SELECT Total_Score / Score_Count " \
+                     "FROM (SELECT * FROM Product) a WHERE a.Product_Code = %s"
+update_rating_sql = "INSERT INTO Reviews( Product_Code, Reviews, Customer_ID, Score) VALUES ( %s, %s, %s, %s)"
+update_web_sql = "INSERT INTO Website( Name, Category) VALUES ( %s, %s)"
 get_rating_sql = "SELECT r.Reviews, c.User_Name, r.Score FROM Reviews r JOIN Customer c ON r.Customer_ID = c.Customer_ID" \
                  " WHERE Product_Code = %s"
 get_products_name_sql = "SELECT * FROM Product WHERE Name LIKE "
@@ -30,18 +36,47 @@ get_products_image_sql = "SELECT * FROM Product WHERE Image_Url LIKE "
 get_products_pcode_sql = "SELECT * FROM Product WHERE Product_Code = %s"
 get_products_ptype_sql = "SELECT * FROM Product WHERE Product_Type LIKE "
 get_products_cat_sql = "SELECT * FROM Product WHERE Category = %s"
+get_site_sql = "SELECT Category FROM Website WHERE Name = %s"
 
+def get_site(a):
+    db_cursor.execute(get_site_sql, (a,))
+    res = []
+    for b in db_cursor.fetchall():
+        res.append(b[0])
+    return res
+
+def check_product(a):
+    db_cursor.execute(check_product_sql,(a,))
+    res = []
+    for b in db_cursor.fetchall():
+        res.append(b[0])
+    return res
+
+def update_web(a,b):
+    db_cursor.execute(update_web_sql, (a, b))
+    db_connection.commit()
+
+def update_product(a):
+    db_cursor.execute(update_product_sql, (a, a))
+    db_connection.commit()
 
 def get_customer(a):
     db_cursor.execute(get_customer_sql, (a,))
     customer = []
     for c in db_cursor.fetchall():
         customer.append(c[0])
-    return customer[0][0]
+    return customer
 
 
-def login(a, b):
-    db_cursor.execute(get_login_sql, (a, b))
+def login_cus(a, b):
+    db_cursor.execute(get_login_cus_sql, (a, b))
+    logins = []
+    for login in db_cursor.fetchall():
+        logins.append({"user": login[0], "pass": login[1]})
+    return logins
+
+def login_mer(a, b):
+    db_cursor.execute(get_login_mer_sql, (a, b))
     logins = []
     for login in db_cursor.fetchall():
         logins.append({"user": login[0], "pass": login[1]})
@@ -50,6 +85,7 @@ def login(a, b):
 
 def update_rating(a, b, c, d):
     db_cursor.execute(update_rating_sql, (a, b, c, d))
+    db_connection.commit()
 
 
 def get_rating(name):
@@ -60,8 +96,8 @@ def get_rating(name):
     return ratings
 
 
-def get_products_name(name):
-    get_products_names_sql = get_products_name_sql + "'%" + name + "%'"
+def get_products_name(name,nam):
+    get_products_names_sql = get_products_name_sql + "'%" + name + "%' " + "AND Category = " + "'" + nam + "'"
     db_cursor.execute(get_products_names_sql)
     products = []
     for product in db_cursor.fetchall():
